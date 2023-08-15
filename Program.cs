d25c0b1629279bbde5261ad63ca76416e836c2c7
RusUsf <ruslan@usf.edu>
2023-08-14 20:19:40 -0400

    Started new Repository - Quizzes

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+// Q1: How to write a "Hello, World" program?
+Console.WriteLine("Hello, World!");
+
+// Q2: How to declare and initialize an integer variable?
+int myNumber = 5;
+Console.WriteLine($"int myNumber = 5; // Prints {myNumber}");
+
